59dc1461056f0c264b1db9609f7845dd50a8dae6
Steven-sxl <shi.xiaolin@hotmail.com>
2020-02-12 15:53:21 +0800

    java和Scala实现flink 的transformation操作

---
+++ src/main/scala/com/sxl/flink/datastream/DataStreamTransformationApp.scala
@@ -0,0 +1,60 @@
+package com.sxl.flink.datastream
+
+import java.{lang, util}
+
+import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
+import org.apache.flink.api.scala._
+import org.apache.flink.streaming.api.collector.selector.OutputSelector
+/**
+  * Date: 2020/2/12 15:17
+  * Author: sxl
+  */
+object DataStreamTransformationApp {
+
+  def main(args: Array[String]): Unit = {
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+
+//    filterFunction(env)
+//    unionFunction(env)
+    splitSelectFunction(env)
+    env.execute("DataStreamTransformationApp")
+  }
+
+  def splitSelectFunction(env:StreamExecutionEnvironment):Unit={
+    val data = env.addSource(new CustomNonParallelSourceFunction)
+
+    val splits = data.split(new OutputSelector[Long] {
+      override def select(value: Long): lang.Iterable[String] ={
+        val list = new util.ArrayList[String]()
+        if (value %2 ==0){
+          list.add("even")
+        }else{
+          list.add("odd")
+        }
+        list
+      }
+    })
+
+    splits.select("even").print().setParallelism(1)
+  }
+
+  def unionFunction(env:StreamExecutionEnvironment):Unit ={
+    val data1 = env.addSource(new CustomNonParallelSourceFunction)
+    val data2 = env.addSource(new CustomNonParallelSourceFunction)
+
+    data1.union(data2).print().setParallelism(1)
+  }
+
+  /**
+    * DataStream 的filter 和map 的使用
+    * @param env
+    */
+  def filterFunction(env:StreamExecutionEnvironment):Unit ={
+    val data = env.addSource(new CustomNonParallelSourceFunction)
+
+    data.map(x=>{
+      println("received:"+ x)
+      x
+    }).filter(_%2 == 0).print().setParallelism(1)
+  }
+}
